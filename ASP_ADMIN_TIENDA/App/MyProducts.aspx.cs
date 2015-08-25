793beb13d0b0ecb305804dc3fb262b2f19de9c2c
fernetsystem <fernetsystem@gmail.com>
2015-08-25 03:01:57 -0500

    Llenar el GridView con c#

---
+++ ASP_ADMIN_TIENDA/App/MyProducts.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ASP_ADMIN_TIENDA.App
+{
+    public partial class MyProducts : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(@"Data Source=SYSTEMXHT\SQLEXPRESS; Initial Catalog=asp;Persist Security Info=True; User ID=sa;Password=w9w9dorotea");
+            SqlDataAdapter da = new SqlDataAdapter("select * from prendas", con);
+            DataSet ds = new DataSet();
+            con.Open();
+            da.Fill(ds);
+            con.Close();
+            GridView1.DataSource = ds.Tables[0];
+            GridView1.DataBind();
+ 
+        }
+    }
+}
